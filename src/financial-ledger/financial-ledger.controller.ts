4c488a38ebd0850fd8b6d85ff8263ae5f9d2f05c
areum <reumaco99@gmail.com>
2022-07-07 22:54:07 +0900

    Feature: Swagger 추가
    FinancialLedgerController, FinancialLedgerWriteDto에 스웨거 데코레이터 추가

--- src/financial-ledger/financial-ledger.controller.ts
+++ src/financial-ledger/financial-ledger.controller.ts
@@ -15,7 +15,9 @@ import { User as CurrentUser } from '../auth/user.decorator';
 import { FinancialLedgerWriteDto } from './dto/financial-ledger.write.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { User } from 'src/entities/User';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('FinancialLedger')
 @UseInterceptors(FinancialLedgerInterceptor)
 @Controller('financial-ledgers')
 export class FinancialLedgerController {
@@ -26,6 +28,8 @@ export class FinancialLedgerController {
   //가계부 작성
   @Post()
   @HttpCode(201)
+  @ApiOperation({ summary: '가계부 내역 작성' })
+  @ApiBody({ type: FinancialLedgerWriteDto })
   @UseGuards(JwtAuthGuard)
   async createMemo(
     @Body() financialLedgerWriteDto: FinancialLedgerWriteDto,
@@ -40,6 +44,7 @@ export class FinancialLedgerController {
   //작성한 가계부 리스트 전부
   @Get()
   @HttpCode(200)
+  @ApiOperation({ summary: '가계부 전체 리스트 조회' })
   @UseGuards(JwtAuthGuard)
   async getAllMemo(@CurrentUser() user: User) {
     const result = await this.financialLedgerService.getAllMemo(user.id);
@@ -49,6 +54,7 @@ export class FinancialLedgerController {
   //가계부 1개의 내역 상세 확인
   @Get('/:id')
   @HttpCode(200)
+  @ApiOperation({ summary: '가계부 내역 상세 확인' })
   @UseGuards(JwtAuthGuard)
   async getOneMemo(@Param('id') id: number, @CurrentUser() user: User) {
     const result = await this.financialLedgerService.getOneMemo(id, user.id);
