e976dc927836890a95f23793549a59a2c16201c7
myeongil <username1103@naver.com>
2022-07-07 12:19:57 +0900

    Test: financal-ledger.service.unit.spec.ts
    - update 테스트 케이스 수정

--- src/financial-ledger/__test__/financial-ledger.service.unit.spec.ts
+++ src/financial-ledger/__test__/financial-ledger.service.unit.spec.ts
@@ -1,7 +1,8 @@
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { anything, instance, mock, when } from 'ts-mockito';
 import { FinancialLedgerRepository } from '../financial-ledger.repository';
 import { FinancialLedgerService } from '../financial-ledger.service';
+import { getFinancialLedger } from './financial-ledger.fixture';
 import { getUser } from './user.fixture';
 
 describe('Financial-Ledger Service Unit Test', () => {
@@ -36,11 +37,47 @@ describe('Financial-Ledger Service Unit Test', () => {
         instance(financialRepository),
       );
 
+      const updatedInfo = {
+        income: 10000,
+        expenditure: 0,
+        remarks: 'updated',
+      };
       // when
-      const result = financialService.delete(user, 123);
+      const result = financialService.update(user, 123, updatedInfo);
 
       // then
       await expect(result).rejects.toThrow(NotFoundException);
+    });
+
+    test('해당하는 가계부 내역이 이미 삭제되었으면 BadRequestException 발생하는가', async () => {
+      // given
+      const user = getUser();
+      const financialLedger = getFinancialLedger({ user });
+      financialLedger.delete();
+
+      financialRepository = mock(FinancialLedgerRepository);
+      when(financialRepository.findOneBy(anything())).thenResolve(
+        financialLedger,
+      );
+      financialService = new FinancialLedgerService(
+        instance(financialRepository),
+      );
+
+      const updatedInfo = {
+        income: 10000,
+        expenditure: 0,
+        remarks: 'updated',
+      };
+
+      // when
+      const result = financialService.update(
+        user,
+        financialLedger.id,
+        updatedInfo,
+      );
+
+      // then
+      await expect(result).rejects.toThrow(BadRequestException);
     });
   });
 });
