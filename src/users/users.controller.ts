7d0a73332759771748166d49b273611e41013a72
juyeop <shininggiver@gmail.com>
2022-07-08 17:34:25 +0900

    Update: 유저 API 스웨거 설정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,24 @@
 import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiHeader, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { User } from 'src/auth/user.decorator';
 import { UserAgent } from './decorators/user-agent.decorator';
 import { LogInBodyDto } from './dto/request/log-in-body.dto';
 import { SingUpBodyDto } from './dto/request/sign-up-body.dto';
+import { ResponseTokenLogOut } from './dto/response/common/response-token-logout.dto';
 import { ResponseToken } from './dto/response/common/response-token.dto';
 import { UsersService } from './users.service';
 
+@ApiTags('User')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: '회원 가입' })
+  @ApiBody({ type: SingUpBodyDto })
+  @ApiCreatedResponse({
+    type: null
+  })
   @HttpCode(201)
   @Post('sign-up')
   async signUp(@Body() signUpBodyDto: SingUpBodyDto) {
@@ -18,6 +26,12 @@ export class UsersController {
     return null;
   }
 
+  @ApiOperation({ summary: '로그인' })
+  @ApiHeader({ name: 'User-Agent', description: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36' })
+  @ApiBody({ type: LogInBodyDto })
+  @ApiOkResponse({
+    type: ResponseToken
+  })
   @HttpCode(200)
   @Post('log-in')
   async login(
@@ -32,6 +46,11 @@ export class UsersController {
     return new ResponseToken(accessToken, refreshToken);
   }
 
+  @ApiOperation({ summary: '로그아웃' })
+  @ApiHeader({ name: 'User-Agent', description: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36' })
+  @ApiOkResponse({
+    type: ResponseTokenLogOut
+  })
   @UseGuards(JwtAuthGuard)
   @HttpCode(200)
   @Post('log-out')
